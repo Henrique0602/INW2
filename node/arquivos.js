d24904881e5ea42b92a8f885e9175c39e2040550
Logon Aluno <logonrmlocal@fiap.com.br>
2023-08-23 09:01:37 -0300

    NOde1

---
+++ node/arquivos.js
@@ -0,0 +1,17 @@
+const {error} = require('console')
+let {readFile, writeFile}=require('fs');
+
+readFile("arquivo.txt","utf-8",(error,texto)=>{
+    if(error){
+        throw error;
+    }else{
+        console.log(texto)
+    }
+})
+    writeFile("arquivo.txt", "Texto feito por write file",(error)=>{
+        if(error){
+             throw error;
+        }else{
+             console.log("Escreveu da forma correta")
+         }
+})
