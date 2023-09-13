4e069b609e647fd508a1cf2f558fdba89b6a6734
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-13 10:02:43 -0300

    Promises

---
+++ promises/exemplo1.js
@@ -0,0 +1,16 @@
+// criar a promises, dentro de uma função
+
+function falarDepoisDe(segundos, frase){
+    return new Promise((resolve,reject) =>{
+        setTimeout(()=>{
+            resolve(frase);
+        }, segundos * 1000);
+    });
+}
+
+
+//chamada da função 
+
+falarDepoisDe(4,"Teste de promises no 2mib ")
+.then(frase => frase.concat("programando em vscode!!!"))
+.then(novafrase => console.log(novafrase))
